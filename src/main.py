e16acbfbc87bfbd35908f49a679922b2197d5e20
Naeim Khan <naeimkhan.ist@gmail.com>
2026-03-01 20:57:58 +0600

    Add multiply function

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ from datetime import date
 print("Naeim Khan")
 print("Today's date:", date.today())
 
-# add fuction call
-from utils import add, subtract
+# add/subtract/multiply fuction call
+from utils import add, subtract, multiply
 print("2 + 3 =", add(2, 3))
 print("5 - 2 =", subtract(5, 2))
+print("2 * 3 =", multiply(2, 3))
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,11 @@
+# Adding
 def add(a, b):
     return a + b
+
+# Subtracting
 def subtract(a, b):
     return a - b
+
+# Multipling
+def multiply(a, b):
+    return a * b
