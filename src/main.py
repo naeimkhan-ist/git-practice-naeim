0d23c7bd48d352ae5531abf783c1d06fa20bc259
Naeim Khan <naeimkhan.ist@gmail.com>
2026-03-01 20:31:15 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
+# date fuction call
 from datetime import date
 print("Naeim Khan")
 print("Today's date:", date.today())
+
+# add fuction call
+from utils import add, subtract
+print("2 + 3 =", add(2, 3))
+print("5 - 2 =", subtract(5, 2))
